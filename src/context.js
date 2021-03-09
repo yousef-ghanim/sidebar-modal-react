e382d52d2bb39c23884f9af57b29b4cf0d5a5702
yousef-ghanim <yousefghanim85@gmail.com>
2021-03-09 17:12:48 +0000

    fixing format

--- src/context.js
+++ src/context.js
@@ -1,40 +1,42 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext } from "react";
 
 const AppContext = React.createContext();
 
+const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-const AppProvider = ({children}) => {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-    const [isModalOpen, setIsModalOpen] = useState(false);
-
-    const openSidebar = () => {
+  const openSidebar = () => {
     setIsSidebarOpen(true);
-    };
-    const closeSidebar = () => {
+  };
+  const closeSidebar = () => {
     setIsSidebarOpen(false);
-    };
-    const openModal = () => {
+  };
+  const openModal = () => {
     setIsModalOpen(true);
-    };
-    const closeModal = () => {
+  };
+  const closeModal = () => {
     setIsModalOpen(false);
-    };
+  };
 
-    return (
-    <AppContext.Provider value={{
+  return (
+    <AppContext.Provider
+      value={{
         isModalOpen,
         isSidebarOpen,
         openModal,
         openSidebar,
         closeModal,
         closeSidebar,
-    }}>{children}</AppContext.Provider>
-    );
-
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 export const useGlobalContext = () => {
-    return useContext(AppContext)
+  return useContext(AppContext);
 };
 
-export {AppContext, AppProvider};
+export { AppContext, AppProvider };
